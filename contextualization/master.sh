ada416efc7160df1872b368a1bfa6e889934a299
Gallon Song <GallonSong@users.noreply.github.com>
2020-10-06 22:41:12 +0200

    Update master.sh
    add more steps and notices for provisioning the master node and running the service

--- contextualization/master.sh
+++ contextualization/master.sh
@@ -5,7 +5,8 @@ sudo apt install -y python3-openstackclient
 
 sudo apt-get install -y rabbitmq-server
 pip3 install –upgrade pip
-pip3 install celery==4.1.1[redis]
+pip3 install celery==4.1.1
+pip3 install celery[redis]
 pip3 install flask
 pip3 install flower
 
@@ -15,5 +16,17 @@ sudo rabbitmqctl add_user js js
 sudo rabbitmqctl add_vhost jsvhost
 sudo rabbitmqctl set_permissions -p jsvhost js ".*" ".*" ".*"
 
+# install redis https://redis.io/topics/quickstart
+# put files under ~/ACC_lab3/data/
+# update master IP in `celeryconfig.py`
+# open ports for rabbitmq and redis
+
 git clone https://github.com/GallonSong/ACC_lab3.git
+celery worker -A tasks --workdir /home/ubuntu/ACC_lab3/ -D
 celery flower -A tasks --workdir /home/ubuntu/ACC_lab3/ --port=8080
+
+export FLASK_APP=twitter.py
+flask run
+
+# ps aux | grep **
+# kill @@@
